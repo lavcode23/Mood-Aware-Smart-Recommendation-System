dd35520593cce1350d5ec588a8acf3a84e4bc1fe
Lavisha Yadav <117895987+lavcode23@users.noreply.github.com>
2025-12-14 14:35:53 +0530

    Create mood_map.py

---
+++ mood_map.py
@@ -0,0 +1,7 @@
+mood_keywords = {
+    "Happy": ["fun", "exciting", "energetic"],
+    "Sad": ["calm", "peaceful", "emotional"],
+    "Stressed": ["relaxing", "calm", "slow"],
+    "Motivated": ["energetic", "action", "motivational"],
+    "Bored": ["fun", "adventure", "exciting"]
+}
